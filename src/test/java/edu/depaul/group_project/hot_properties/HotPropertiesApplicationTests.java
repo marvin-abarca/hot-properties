0f354a1ae5ca1ba96ae4af5a8be184671cba9d6b
Marvin Abarca <marvinabarca0@gmail.com>
2025-05-24 14:01:17 -0500

    init push

---
+++ src/test/java/edu/depaul/group_project/hot_properties/HotPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.depaul.group_project.hot_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
