4a73356142590dbdb4e8268d6707129cb5b7d108
Marvin Abarca <marvinabarca0@gmail.com>
2025-05-24 21:50:10 -0500

    adding Exception classes

---
+++ src/main/java/edu/depaul/group_project/hot_properties/exceptions/InvalidPropertyParameterException.java
@@ -0,0 +1,7 @@
+package edu.depaul.group_project.hot_properties.exceptions;
+
+public class InvalidPropertyParameterException extends RuntimeException{
+    public InvalidPropertyParameterException(String message) {
+        super(message);
+    }
+}
